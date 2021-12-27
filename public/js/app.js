52f8a02c889f83d9a87ac24c4bc5654db17b8f53
Nithish-277 <44161030+Nithish-277@users.noreply.github.com>
2021-12-27 21:09:52 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-// const { response } = require("express")
-
-console.log('Client side Javascript file is loaded')
-
 fetch('https://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
         console.log(data)
